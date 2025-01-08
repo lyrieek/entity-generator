f30541ad54b65bcf6ee7d8b3e39c6ce0574c1495
lyrieek <lyrieek@hotmail.com>
2025-01-08 10:31:32 +0800

    :construction: reset GradlePlugin

--- src/main/java/com/lyrieek/eg/gradle/GradlePlugin.java
+++ src/main/java/com/lyrieek/eg/gradle/GradlePlugin.java
@@ -5,18 +5,28 @@ import org.gradle.api.Project;
 import org.gradle.api.tasks.TaskProvider;
 
 public class GradlePlugin implements Plugin<Project> {
+
+	private String dbConfigPath;
+
 	@Override
 	public void apply(Project project) {
-		PluginParam param = project.getExtensions().create("myPluginConfig", PluginParam.class, project);
+		GradlePlugin entityGenerator = project.getExtensions().create("entityGenerator", GradlePlugin.class);
 
 		TaskProvider<?> printTask = project.getTasks().register("entityGenerator", task -> {
 			task.doLast(taskAction -> {
-				System.out.println("db config: " + param.getDbConfigPath());
+				System.out.println("db config path: " + entityGenerator.getDbConfigPath());
 			});
 		});
 
 		// 将打印任务添加到编译任务之前
 		project.getTasks().named("compileJava").configure(task -> task.dependsOn(printTask));
+	}
 
+	public String getDbConfigPath() {
+		return dbConfigPath;
+	}
+
+	public void setDbConfigPath(String dbConfigPath) {
+		this.dbConfigPath = dbConfigPath;
 	}
 }
